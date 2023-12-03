4d500e87feb5552ff1b2c99ac5d3780eaa01771a
Juan Pinto <j.pintos2345@gmail.com>
2023-12-03 14:23:56 -0300

    Secrea Base de Datos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    
+   Nombre = models.CharField(max_length=150)
+   Apellido = models.CharField(max_length=150)
+   Usuario = models.CharField(max_length=10)
+   Clave = models.CharField(max_length=50)
+   Correo = models.CharField(max_length=150)
+ 
